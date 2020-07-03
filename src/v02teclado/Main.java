b428f110b367ecb6f9829ebe9086eb545c8ef4d0
brunolopeschagas <brunolopeschagas@gmail.com>
2020-07-03 09:40:20 -0300

    escrevendo sozinho

--- src/v02teclado/Main.java
+++ src/v02teclado/Main.java
@@ -1,7 +1,6 @@
 package v02teclado;
 
 import java.awt.Robot;
-import java.awt.event.KeyEvent;
 
 public class Main {
 
@@ -9,25 +8,9 @@ public class Main {
 
 		Robot robo = new Robot();
 		robo.delay(2000);
-				
-//		robo.keyPress(KeyEvent.VK_SHIFT);
-//		robo.keyPress(KeyEvent.VK_B);
-//		robo.keyRelease(KeyEvent.VK_B);
-//		robo.keyPress(KeyEvent.VK_L);
-//		robo.keyRelease(KeyEvent.VK_L);
-//		robo.keyPress(KeyEvent.VK_S);
-//		robo.keyRelease(KeyEvent.VK_S);
-//		robo.keyRelease(KeyEvent.VK_SHIFT);
-//		robo.keyPress(KeyEvent.VK_O);
-//		robo.keyRelease(KeyEvent.VK_O);
-//		robo.keyPress(KeyEvent.VK_F);
-//		robo.keyRelease(KeyEvent.VK_F);
-//		robo.keyPress(KeyEvent.VK_T);
-//		robo.keyRelease(KeyEvent.VK_T);
 		
-		RoboSoletrando roboSoletrando = new RoboSoletrando(robo);	
+		RoboSoletrando roboSoletrando = new RoboSoletrando(robo);
 		roboSoletrando.escrever("BLSoft Desenvolvimento de Sistemas. A numero 1");
-		
 		
 	}
 	
